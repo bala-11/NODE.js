6422a56339655622991d39cc8abb445fcb4045e4
Balaji <balaji.kalimuthu@mphasis.com>
2024-11-27 17:12:36 +0530

    learned about memoisation

--- memoziseFunction.js
+++ memoziseFunction.js
@@ -1,11 +1,11 @@
 const delay = (time)=>
     new Promise((res) => setTimeout(res,time))
 
-async function getData(id) {
+async function getData(id=1) {
     const url = `https://jsonplaceholder.typicode.com/todos/${id}`
     const response = await fetch(url)
     const json = await response.json()
-    await delay(4000);
+    await delay(5000)
     return json.title
 }
 
@@ -14,22 +14,16 @@ const cachedFn = (fn) =>{
     const map = new Map()
     return async function (...args) {
         const now = performance.now()
-        console.log(now);
         const key = args.toString()
-        console.log(key);
-        
         if(map.has(key)){
             const response = await map.get(key)
             const later = performance.now()
-            console.log("From Map");
-            console.log(later);
-            return `response ${response} time is ${later - now}`
+            return `cache response ${response} time is ${later - now}\n`
         }
         const response = fn(...args)
         map.set(key,response)
         const later = performance.now()
-        console.log(later)
-        return `response ${response} time is ${later - now}`
+        return `response ${response} time is ${later - now}\n`
     }
 }
 
