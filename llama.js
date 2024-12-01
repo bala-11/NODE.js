0ed985d0bcfd855c7e89391ba1aba78aba9209fe
bala-11 <balajikalimuthu482@gmail.com>
2024-12-01 14:47:11 +0530

    pending llama

---
+++ llama.js
@@ -0,0 +1,24 @@
+import { HfInference } from "@huggingface/inference";
+
+const client = new HfInference("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");
+
+let out = "";
+
+const stream = client.chatCompletionStream({
+	model: "google/gemma-2-2b-it",
+	messages: [
+		{
+			role: "user",
+			content: "What is the capital of France?"
+		}
+	],
+	max_tokens: 500
+});
+
+for await (const chunk of stream) {
+	if (chunk.choices && chunk.choices.length > 0) {
+		const newContent = chunk.choices[0].delta.content;
+		out += newContent;
+		console.log(newContent);
+	}  
+}
