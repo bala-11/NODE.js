c5cacfd8413c09aa3a9950dc163980acb70e1a7c
bala-11 <balajikalimuthu482@gmail.com>
2025-01-16 08:16:13 +0530

    Added chat and IVR automation files

--- dummy.js
+++
@@ -1,9 +0,0 @@
-var reporterLastName = context.entities.entReporterLastNameComposite?.entReporterLastName?  context.entities.entReporterLastNameComposite?.entReporterLastName? : " ";
-koreDebugger.log(reporterName)
-
-if(reporterName && reporterName.length != 0 && reporterName.split(" ").length <= 1){
-    koreDebugger.log("PARTIAL NAME CAPTURED :::::")
-    context.reporterPath = "PARTIALNAME"
-}
-
-koreDebugger.log("FULL NAME CAPTURED :::::")
